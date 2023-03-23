a917beebcee4ef1df17951899160321940a7c680
Tristian Barrett <trbarrett@gmail.com>
2023-03-23 13:47:27 +0000

    Created controls for Region and Lambda

--- src/Program.fs
+++ src/Program.fs
@@ -2,6 +2,7 @@ namespace Avaloniaui.Demo.Program
 
 open Avalonia
 open Avalonia.Controls.ApplicationLifetimes
+open Avalonia.Media
 open Avalonia.Themes.Fluent
 open Avalonia.FuncUI.Hosts
 open Avalonia.Controls
@@ -17,26 +18,68 @@ module Main =
 
             DockPanel.create [
                 DockPanel.children [
-                    Button.create [
-                        Button.dock Dock.Bottom
-                        Button.onClick (fun _ -> state.Set(state.Current - 1))
-                        Button.content "-"
-                        Button.horizontalAlignment HorizontalAlignment.Stretch
-                        Button.horizontalContentAlignment HorizontalAlignment.Center
+                    DockPanel.create [
+                        DockPanel.dock Dock.Top
+                        DockPanel.children [
+                            TextBlock.create [
+                                TextBlock.margin (Thickness(10))
+                                TextBlock.verticalAlignment VerticalAlignment.Center
+                                TextBlock.textAlignment TextAlignment.Right
+                                TextBlock.dock Dock.Left
+                                TextBlock.width 50
+                                TextBlock.text "Region:"
+                            ]
+                            ComboBox.create [
+                                ComboBox.margin (Thickness(10))
+                                ComboBox.verticalAlignment VerticalAlignment.Center
+                                ComboBox.dock Dock.Left
+                                ComboBox.width 250
+                                ComboBox.dataItems [
+                                    "Europe (Ireland) eu-west-1"
+                                    "Europe (London) eu-west-2"
+                                ]
+                            ]
+                            TextBlock.create [
+                                TextBlock.margin (Thickness(10))
+                                TextBlock.verticalAlignment VerticalAlignment.Center
+                                TextBlock.textAlignment TextAlignment.Right
+                                TextBlock.dock Dock.Left
+                                TextBlock.width 70
+                                TextBlock.text "Lambda:"
+                            ]
+                            TextBox.create [
+                                TextBox.margin (Thickness(10))
+                                TextBox.verticalAlignment VerticalAlignment.Center
+                                TextBox.dock Dock.Right
+                            ]
+                        ]
                     ]
-                    Button.create [
-                        Button.dock Dock.Bottom
-                        Button.onClick (fun _ -> state.Set(state.Current + 1))
-                        Button.content "+"
-                        Button.horizontalAlignment HorizontalAlignment.Stretch
-                        Button.horizontalContentAlignment HorizontalAlignment.Center
-                    ]
-                    TextBlock.create [
-                        TextBlock.dock Dock.Top
-                        TextBlock.fontSize 48.0
-                        TextBlock.verticalAlignment VerticalAlignment.Center
-                        TextBlock.horizontalAlignment HorizontalAlignment.Center
-                        TextBlock.text (string state.Current)
+                    // TODO - Vertical GridSplitter for Request/Response
+                    DockPanel.create [
+                        DockPanel.dock Dock.Bottom
+                        DockPanel.children [
+                            Button.create [
+                                Button.dock Dock.Bottom
+                                Button.onClick (fun _ -> state.Set(state.Current - 1))
+                                Button.content "-"
+                                Button.horizontalAlignment HorizontalAlignment.Stretch
+                                Button.horizontalContentAlignment HorizontalAlignment.Center
+                            ]
+                            Button.create [
+                                Button.dock Dock.Bottom
+                                Button.onClick (fun _ -> state.Set(state.Current + 1))
+                                Button.content "+"
+                                Button.horizontalAlignment HorizontalAlignment.Stretch
+                                Button.horizontalContentAlignment HorizontalAlignment.Center
+                            ]
+                            TextBlock.create [
+                                TextBlock.dock Dock.Top
+                                TextBlock.fontSize 48.0
+                                TextBlock.verticalAlignment VerticalAlignment.Center
+                                TextBlock.horizontalAlignment HorizontalAlignment.Center
+                                TextBlock.text (string state.Current)
+                            ]
+                        ]
                     ]
                 ]
             ]
@@ -46,8 +89,9 @@ type MainWindow() =
     inherit HostWindow()
     do
         base.Title <- "Direktiv"
-        base.ClientSize <- Size(300, 420)
+        base.ClientSize <- Size(800, 420)
         base.Content <- Main.view ()
+        //base.ExtendClientAreaToDecorationsHint <- true
 
 type App() =
     inherit Application()
